8ae9eab9eeb0babfbbfbe181409deb0b647c9ea3
Mohan Lal <mhnuk2007@gmail.com>
2026-02-10 22:05:28 +0500

    udate

---
+++ js-practice/variable.js
@@ -0,0 +1,15 @@
+var name = "Honey";
+//name = "Honey Chauhan"
+var name = "Honey Chauhan";
+console.log(name);
+
+//
+let name1 = "Honey";
+// let name1 = "Honey Chauhan"; //not allowed
+name1 = "Honey Chauhan";
+console.log(name1);
+
+const name2 = "Honey";
+//const name2 = "Honey Chauhan"; //not allowed 
+//name2 = "Honey Chauhan"; //not allowed
+console.log(name2);
