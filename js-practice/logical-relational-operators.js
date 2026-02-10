15c3730e713c0fdd95db464c4088b6214ba18441
Mohan Lal <mhnuk2007@gmail.com>
2026-02-11 00:04:17 +0500

    Add logical and relational operator example

---
+++ js-practice/logical-relational-operators.js
@@ -0,0 +1,11 @@
+let age = 20;
+let hasID = true;
+
+// Relational operators
+console.log(age >= 18);   // true
+console.log(age < 18);    // false
+
+// Logical operators
+console.log(age >= 18 && hasID); // true (both true)
+console.log(age >= 18 || hasID); // true (at least one true)
+console.log(!(age >= 18));       // false (NOT operator)
