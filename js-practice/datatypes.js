d012ad49cc9c7ca18f95d51d1c1611f39598b0e1
Mohan Lal <mhnuk2007@gmail.com>
2026-02-10 22:49:56 +0500

    Add data type examples

---
+++ js-practice/datatypes.js
@@ -0,0 +1,25 @@
+let age = 25;
+console.log(typeof age); // "number"
+
+let name = "Honey";
+console.log(typeof name); // "string"
+
+let isActive = true;
+console.log(typeof isActive); // "boolean"
+
+let person = {name: "Honey"};
+console.log(typeof person); // "object"
+
+let emptyValue = null;
+console.log(typeof emptyValue); // "object"
+
+
+let arr = [1,2,3];
+console.log(typeof arr); // "object" (arrays are objects)
+
+
+console.log(typeof greet); 
+
+function greet(){
+    console.log("Hello");
+}
